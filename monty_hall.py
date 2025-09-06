65b442cb297f76b4aab70217511976e844cb47e1
diegao <diegonoguerarec@gmail.com>
2025-09-05 22:05:49 -0300

    Iterations with door change

--- monty_hall.py
+++ monty_hall.py
@@ -2,8 +2,8 @@ import random
 
 iterations = 1000
 
-# Number of wins
-wins = 0
+# Number of wins with NO door change
+wins_NO = 0
 
 # Iterations with NO door change
 for i in range(iterations):
@@ -29,7 +29,7 @@ for i in range(iterations):
         # Ask participant if he wants to change door
         # Participant says NO
         # Since participant already choose correct door
-        wins = wins + 1 # Participant wins
+        wins_NO += 1 # Participant wins
 
     #else: # If participant choose incorrect door
         # Open the incorrect door left and ask participand if he wants to change door
@@ -39,4 +39,45 @@ for i in range(iterations):
         # Since participant already choose incorrect door
         # Pasticipant does not win
 
-print(wins)
+
+# Number of wins with door change
+wins_YES = 0
+
+# Iterations with door change
+for i in range(iterations):
+
+    # Represents three doors
+    doors = [0, 0, 0]
+
+    # Choose random door to put the price
+    door_number = random.choice([0, 1, 2])
+
+    # Putting the price (Set door to 1)
+    # 1: Price behind the door
+    # 0: No price
+    # -1: Opened door
+    doors[door_number] = 1
+
+    # Random door choice from the participant
+    initial_choice = random.choice([0, 1, 2])
+
+    if doors[initial_choice] == 1: # If participant choose correct door
+        # Open one of the other doors and ask participant if he wants to change door
+
+        # Ask participant if he wants to change door
+        # Participant says YES
+        # Since participant first choose correct door and changes it, now he is on incorrect door
+        # Participant does not win
+        pass
+
+    else: # If participant choose incorrect door
+        # Open the incorrect door left and ask participand if he wants to change door
+
+        # Ask participant if he wants to change door
+        # Participant says YES
+        # Since participant first choose incorrect door and changes it, now he is on correct door
+        # Pasticipant wins
+        wins_YES += 1
+
+print(wins_NO)
+print(wins_YES)
